9a0901b40091a679e43f386a4c3f2670cc84f693
Yan Alves <yanalves@hotmail.com>
2020-06-07 02:29:57 -0300

    :construction: Finaliza busca de itens da API

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
+import api from '../../services/api';
+
+interface Item {
+    id: number,
+    title: string,
+    image_url: string
+}
 
 const Points = () => {
     const navigation = useNavigation();
+
+    const [items, setItems] = useState<Item[]>([]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, [])
 
     function handleNavigateBack() {
         navigation.goBack();
@@ -15,6 +31,18 @@ const Points = () => {
 
     function handleNavigateToDetail() {
         navigation.navigate('Detail');
+    }
+
+    function handleSelectItem(id: number) {
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if (alreadySelected >= 0){
+            const filteredItems = selectedItems.filter(item => item !== id);
+
+            setSelectedItems(filteredItems);
+        } else {
+            setSelectedItems([ ...selectedItems, id ]);
+        }
     }
 
     return (
@@ -50,24 +78,20 @@ const Points = () => {
                     showsHorizontalScrollIndicator={false}
                     contentContainerStyle={{ paddingHorizontal: 30 }}
                 >
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri={''}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri={''}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri={''}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri={''}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri={''}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri={''}/>
-                    </TouchableOpacity>
+                    {items.map(item => (
+                        <TouchableOpacity 
+                            key={String(item.id)} 
+                            style={[
+                                styles.item, 
+                                selectedItems.includes(item.id) ? styles.selectedItem : {}
+                            ]} 
+                            onPress={() => {handleSelectItem(item.id)}}
+                            activeOpacity={0.6}
+                        >
+                            <SvgUri width={42} height={42} uri={item.image_url}/>
+                            <Text style={styles.itemTitle}>{item.title}</Text>
+                        </TouchableOpacity>
+                    ))}
                 </ScrollView>
             </View>
         </SafeAreaView>
