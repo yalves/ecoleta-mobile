2f59538b2a32228639bdc74a9de765210e9a8831
Yan Alves <yanalves@hotmail.com>
2020-06-07 23:13:16 -0300

    :construction: Altera recebimento de url de imagem

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -17,6 +17,7 @@ interface Item {
 interface Point {
     id: number;
     image: string;
+    image_url: string;
     name: string;
     latitude: number;
     longitude: number;
@@ -124,7 +125,7 @@ const Points = () => {
                                     onPress={() => {handleNavigateToDetail(point.id)}}
                                 >
                                     <View style={styles.mapMarkerContainer}>
-                                        <Image style={styles.mapMarkerImage} source={{uri: point.image}}/>
+                                        <Image style={styles.mapMarkerImage} source={{uri: point.image_url}}/>
                                         <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                                     </View>
                                 </Marker>
